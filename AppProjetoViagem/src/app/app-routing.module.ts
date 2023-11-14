91e861aef061184dd377732894b3192707640b38
José VG <vitor.jose1906@gmail.com>
2023-11-14 15:54:41 -0300

    correcao path

--- AppProjetoViagem/src/app/app-routing.module.ts
+++ AppProjetoViagem/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarroDeAluguelsComponent } from './components/carro-de-aluguels/carro-de-aluguels.component';
 import { CidadesComponent } from './components/cidades/cidades.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { CompanhiasComponent } from './components/companhias/companhias.component';
+import { HospedagemsComponent } from './components/hospedagems/hospedagems.component';
+import { GuiaTuristicosComponent } from './components/guia-turisticos/guia-turisticos.component';
+import { PassagemsComponent } from './components/passagems/passagems.component';
+import { PaisessComponent } from './components/paisess/paisess.component';
 
-const routes: Routes = [{
-  path: 'carrosaluguel', component:CarroDeAluguelsComponent,
-  path: 'cidades', component:CidadesComponent,
-  path: 'carrosaluguel', component:CarroDeAluguelsComponent
-}];
+const routes: Routes = [
+  {path: 'carrosaluguel', component:CarroDeAluguelsComponent},
+  {path: 'cidades', component:CidadesComponent},
+  {path: 'clientes', component:ClientesComponent},
+  {path: 'companhia', component:CompanhiasComponent},
+  {path: 'hospedagem', component:HospedagemsComponent},
+  {path: 'guiaturistico', component:GuiaTuristicosComponent},
+  {path: 'passagem', component:PassagemsComponent},
+  {path: 'paises', component:PaisessComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
